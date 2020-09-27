691f80ace3f20e346cebf435f8c0b0666172f2a1
JJ-183 <jj.bowles@icloud.com>
2020-09-27 19:27:54 +0100

    Added better password creation stored in a separate file and added ability to try again with the same question.

--- doMusicQuiz.py
+++ doMusicQuiz.py
@@ -1,10 +1,38 @@
 import functions
 
-def dothequiz():
-    #Login user and returning the user name
-    user = functions.login()
-    #Do quiz and passing in the username so it can be stored
-    functions.musicQuiz(user)
+def doMusicQuiz(usr):
+    #Welcome message
+    print("Welcome to the music quiz, please login if you have already registered, if not add a user and password and those details will be registered for you to use next time")
+    #Asking for Name
+    uN = input("Name: ")
+    #Asking for Password
+    pW = input("Password: ")
 
+    #Checking if Name and password are already in the user file
+    if uN in usr.keys():
+        if pW == usr[uN]:
+            print("Welcome back {0}".format(uN))
+        else:
+            print("Sorry its seems that you are using an incorrect password or if you are a new user that username is already taken so please pick another")
+            #Try login again
+            return False
+    #If username not found add that and password to the users file
+    else:
+        print("Hello, {0} ! Thanks for registering and enjoy the quiz".format(uN))
+        #Add new username with password
+        usr[uN] = pW
+        #Add the new username with password to the users file
+        functions.writeUsers(usr)
+    #Run the quiz, passing the username being used
+    functions.musicQuiz(uN)
+    return True
 
-dothequiz()
+#Read users from the users file
+users = functions.readUsers()
+
+#Check if entered username and password are already in the users.json file
+success = doMusicQuiz(users)
+while not success:
+    success = doMusicQuiz(users)
+
+#import functions
--- functions.py
+++ functions.py
@@ -1,62 +1,54 @@
 import random
-#Opening the files i need to read, using 'with open' this will close the files at the end of the quiz
-#Songs
+import json
+
+
+# Opening the files i need to read, using 'with open' this will close the files at the end of the quiz
+# Songs
 with open('Songs.txt', 'r') as songsfilecontents:
     list_of_songs = songsfilecontents.readlines()
-#Bands
+# Bands
 with open('bands.txt', 'r') as bandsfilecontents:
     list_of_bands = bandsfilecontents.readlines()
-#Year
+# Year
 with open('yearofsong.txt', 'r') as yearfilecontents:
     list_of_years = yearfilecontents.readlines()
 
-#Creating a random number to use to get a song and band
-random_number = random.randint(0, 19)
-#Getting a random song
+# Creating a random number to use to get a song and band
+random_number = random.randint(0, 7)
+# Getting a random song
 random_song = list_of_songs[random_number]
-#Removing any whitespace after the random song
+# Removing any whitespace after the random song
 random_song = random_song.rstrip("\n")
-#Getting a matching random band
+# Getting a matching random band
 random_band = list_of_bands[random_number]
-#Getting the matching year of the song
+# Getting the matching year of the song
 random_year = list_of_years[random_number]
-#Getting the intials of the random song
-song_initails = "".join(item[0].upper() for item in random_song.split())
+# Getting the intials of the random song
+song_initials = "".join(item[0].upper() for item in random_song.split())
 
-#Functions
-def check_password(u_name, p_word):
-    if u_name == "John" and p_word == "musicquiz":
-        print("Welcome John")
-        return "login_ok"
-    elif u_name == "Jules" and p_word == "musicquiz1":
-        print("Welcome Jules")
-        return "login_ok"
-    elif u_name == "JJ" and p_word == "musicquiz2":
-        print("welcome JJ")
-        return "login_ok"
 
-def login():
-    while True:
-        print("Welcome to the music quiz, please enter your username")
-        #Creating an input for the user name
-        u_name = input("User Name: ")
-        print("Now please enter your password:")
-        #Create an input for the password
-        p_word = input("Password: ")
-        #Checking that its a know user and password combo
-        login = check_password(u_name, p_word)
-        if login == "login_ok":
-            break
-        else:
-            print("Sorry you dont have a valid user / password, please try again")
-    return u_name
+# Functions
+def readUsers():
+    try:
+        with open("users.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def writeUsers(usr):
+    with open("users.json", "w+") as f:
+        json.dump(usr, f)
+
 
 def save_score(score, user):
-    #Making a dict to save the current user and score
+    # Making a dict to save the current user and score
     a_dictionary = {user: score}
-    #Appending the dict to a txt file
+    # Appending the dict to a txt file
     with open('scores.txt', 'a') as fp:
         print(a_dictionary, file=fp)
+        # fp.write(json.dumps(a_dictionary, indent=4))
+
 
 def read_scores(file):
     d = {}
@@ -67,40 +59,44 @@ def read_scores(file):
         d[user] = score
         print(user, score)
 
+
 def musicQuiz(user):
     points = 3
     print("Can you guess the name of the song with just the intials and the band name?, you will have 3 trys")
-    print("The initals of the song are", song_initails, "the band name is", random_band,)
-    clue = input("Would you like a clue?, if so type clue, if not type no ")
-    if (clue == 'clue'):
+    print("The initals of the song are", song_initials, "the band name is", random_band)
+    clue = input("Would you like a clue?, if so type yes, if not type no ")
+    if (clue == 'yes'):
         print("the year the song was released was", random_year)
-    #Adding an input for the user to guess the song
+    # Adding an input for the user to guess the song
     guess_the_song = input("Guess the name of the song! ")
-    #Setting the number of trys to 3
+    # Setting the number of trys to 3
     number_of_trys_left = 2
-    #Defining a wrong answer
+    # Defining a wrong answer
     wrong_answer = (guess_the_song != random_song)
-    #Starting the quiz answer trys
+    # Starting the quiz answer trys
     while number_of_trys_left > 0 and wrong_answer:
         guess_the_song = input("Thats wrong sorry, please try again! ")
         points -= 1
-        #adding a used try
+        # adding a used try
         number_of_trys_left -= 1
         wrong_answer = (guess_the_song != random_song)
-    #Getting it wrong
+    # Getting it wrong
     if wrong_answer:
-        print ("Thats wrong you clown!, you have had your 3 trys now go away!")
-    #Getting it right
+        print("Sorry that's incorrect, you have had your 3 trys now so the quiz is over for you {0}".format(user))
+        #Checking if the player would like to try again
+        playagainafterfail = input("Would you like to try the same question again or quit the quiz? Type same for the same question again, or no to quit ")
+        if (playagainafterfail == 'same'):
+            #Ask same question again, the random_band already used should still be in memory for this session when running this function again
+            musicQuiz(user)
+        else:
+            print("Thanks for playing and goodbye!")
+    # Getting it right
     else:
-        #Getting the final score
+        # Getting the final score
         final_score = points
-        #Saving the final score and the user that played the quiz in a text file
+        # Saving the final score and the user that played the quiz in a text file
         save_score(final_score, user)
-        if number_of_trys_left == 2:
-            print("well done got it on your first try")
-        elif number_of_trys_left == 1:
-            print("well done got it on your second try")
-        elif number_of_trys_left == 0:
-            print("uh oh you got that on your last try!")
+        print("Ohh look at you knowing the right band!, well done!")
         print("You scored {0} point/s".format(final_score))
         read_scores('scores.txt')
+        print("Thanks for playing and goodbye!")
